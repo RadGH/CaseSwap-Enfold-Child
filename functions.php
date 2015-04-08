3bbb8b821373c74975b36478edb67afefb80b02c
Radley <radleygh@gmail.com>
2015-04-08 00:24:12 -0700

    Added dynamic menu function, registered more descriptive custom menus, and unregistered the default menus

--- functions.php
+++ functions.php
@@ -1,11 +1,33 @@
 <?php
 
-// Customize the header configuration used by Enfold.
-// This is primarily to use a different header menu when the user is logged in.
-function caseswap_customize_header( $header ) {
-  echo '<pre>';
-  var_dump( $header );
-  echo '</pre>';
-  exit;
+/*
+ * Gets the menu location key based on whether the user is logged in or not, or another condition.
+ *
+ * Menu locations:
+ *   1. "primary" - Main menu at the top of the page. Replaces "avia"
+ *   2. "secondary" - Displayed at the top of the page (I assume). Replaces "avia2"
+ *   3. "footer" - Displayed in the footer. Replaces "avia3"
+ */
+
+function caseswap_menu_location( $location ) {
+  if ( is_user_logged_in() ) return $location . '-logged_in';
+  else return $location . '-logged_out';
 }
-// add_filter( 'avf_header_setting_filter', 'caseswap_customize_header' );
+
+function caseswap_register_menus() {
+  register_nav_menus(array(
+    'primary-logged_out' => 'Primary (Logged Out)', // replaces "avia"
+    'primary-logged_in' => 'Primary (Logged In)',
+
+    'secondary-logged_out' => 'Secondary (Logged Out)', // replaces "avia2"
+    'secondary-logged_in' => 'Secondary (Logged In)',
+
+    'footer-logged_out' => 'Footer (Logged Out)', // replaces "avia3"
+    'footer-logged_in' => 'Footer (Logged In)',
+  ));
+
+  unregister_nav_menu('avia');
+  unregister_nav_menu('avia2');
+  unregister_nav_menu('avia3');
+}
+add_action( 'after_setup_theme', 'caseswap_register_menus', 15 );
